51f40efb7cf60e48c3fadf8039e92ed4d6639d39
Francisco <fsalazarg@ing.ucsc.cl>
2022-02-09 17:25:20 -0300

    Todo

---
+++ js/iot-mqtt.js
@@ -0,0 +1,41 @@
+var clientId = "ws" + Math.random(); //Dirección aleatoria
+
+// Create a client instance
+client = new Paho.MQTT.Client("IP del WS", "Puerto del WS", clientId);
+
+// set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+
+// connect the client
+client.connect({ onSuccess: onConnect });
+
+// called when the client connects
+function onConnect() {
+  // Once a connection has been made, make a subscription and send a message.
+  console.log("Conectado MQTT-WebSocket");
+  client.subscribe("Temp");
+  client.subscribe("Relay");
+  /*message = new Paho.MQTT.Message("Hello");
+  message.destinationName = "World";
+  client.send(message);*/
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("Conexión Perdida. Error:" + responseObject.errorMessage);
+  }
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+  console.log(message.destinationName + ": " + message.payloadString);
+  if (message.destinationName == 'Temp') {
+    document.getElementById("ValorTemp").textContent = message.payloadString;
+  }
+
+  if (message.destinationName == 'Relay') {
+    document.getElementById("ValorRelay").textContent = message.payloadString;
+  }
+}
